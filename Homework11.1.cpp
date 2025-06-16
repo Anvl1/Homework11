5bce3e3212cb48bc13260a98236d4a91fed41d9d
Anvl1 <88466348+Anvl1@users.noreply.github.com>
2025-06-16 23:43:58 +1000

    Add files via upload

---
+++ Homework11.1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "rus");
+
+    std::string firstName;
+    std::string lastName;
+    
+    std::cout << "Введите имя: ";
+    std::getline(std::cin, firstName);
+    
+    std::cout << "Введите фамилию: ";
+    std::getline(std::cin, lastName);
+    
+    std::cout << "Здравствуйте, " << firstName << " " << lastName << "!" << std::endl;
+
+    return 0;
+}
---
+++ Homework11.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "rus");
+
+    std::string secretWord = "малина";
+    std::string userGuess;
+    do
+    {
+        std::cout << "Угадайте слово: ";
+        std::getline(std::cin, userGuess);
+        if (userGuess != secretWord)
+        {
+            std::cout << "Неправильно" << std::endl;
+        }
+    } while (userGuess != secretWord);
+
+    std::cout << "Правильно! Вы победили! Загаданное слово — " << secretWord << std::endl;
+
+    return 0;
+}
